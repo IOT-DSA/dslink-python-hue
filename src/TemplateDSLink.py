3a07f1d91ed4c14904e24d87e61f8e47ceb95952
Dennis Khvostionov <dennisk@dglogik.com>
2015-11-28 17:31:27 -0800

    Working on it

--- src/TemplateDSLink.py
+++ src/TemplateDSLink.py
@@ -2,6 +2,18 @@ import dslink
 import random
 from twisted.internet import reactor
 
+from phue import Bridge
+from rgb_cie import Converter
+
+
+b = Bridge('192.168.86.188')
+b.connect()
+lights = b.get_light_objects('id')
+for l in b.lights:
+    print(l)
+
+converter = Converter()
+lights[1].xy = converter.hexToCIE1931('ff0000');
 
 class TemplateDSLink(dslink.DSLink):
     def __init__(self, config):
@@ -13,17 +25,24 @@ class TemplateDSLink(dslink.DSLink):
         self.profile_manager.create_profile("set_speed")
         self.profile_manager.register_callback("set_speed", self.set_speed)
 
+        self.profile_manager.create_profile("set_on")
+        self.profile_manager.register_callback("set_on", self.set_on)
+
+        self.profile_manager.create_profile("set_hex")
+        self.profile_manager.register_callback("set_hex", self.set_hex)
+
         reactor.callLater(0.1, self.poll)
 
     def get_default_nodes(self):
         root = self.get_root_node()
 
         set_speed = dslink.Node("set_speed", root)
+        set_speed.set_display_name("Set Poll Rate");
         set_speed.set_parameters([
             {
                 "name": "Speed",
                 "type": "number",
-                "value": 0.1
+                "value": 1
             }
         ])
         set_speed.set_columns([
@@ -48,10 +67,52 @@ class TemplateDSLink(dslink.DSLink):
         root.add_child(test_two)
         root.add_child(set_speed)
 
+        for l in b.lights:
+            node = dslink.Node("light_" + str(l.light_id), root)
+            node.set_display_name(l.name);
+            root.add_child(node)
+
+            set_on = dslink.Node("set_on", root)
+            set_on.set_display_name("@set");
+            set_on.set_parameters([
+                {
+                    "name": "value",
+                    "type": "boolean",
+                    "value": False
+                }
+            ])
+            set_on.set_columns([
+                {
+                    "name": "Success",
+                    "type": "bool",
+                    "value": False
+                }
+            ])
+            set_on.set_profile("set_on")
+            set_on.set_invokable("write")
+
+            node.add_child(set_on)
+
         return root
 
     def set_speed(self, parameters):
-        self.speed = int(parameters.params["Speed"])
+        #self.speed = int(parameters.params["Speed"])
+        try:
+            lights[1].hue = parameters.params["Speed"]
+        except Exception, e:
+            print "Exception: %s" % e
+
+        return [
+            [
+                True
+            ]
+        ]
+
+    def set_on(self, parameters):
+        try:
+            lights[1].hue = parameters.params["Speed"]
+        except Exception, e:
+            print "Exception: %s" % e
 
         return [
             [
